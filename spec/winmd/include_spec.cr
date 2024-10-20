779aa6c4af8cd70723d91598c79d830781869d81
Matthew J. Black <mjblack@gmail.com>
2024-10-20 15:02:35 -0500

    Renamed spec file
    Spec file was using the original names I had started to use,
    and it just needed to be refactored.

--- spec/winmd/include_spec.cr
+++ spec/winmd/include_spec.cr
@@ -1,9 +1,9 @@
 require "../spec_helper"
 require "../../src/winmd"
 
-module WinMD::Template::TInclude
+module WinMD::Template::Include
 
-  describe "WinMD Template TInclude" do
+  describe "WinMD Template Include" do
     # api = "WinMD.Include.Test"
     # it "should create a new object successfully" do
     #   include_obj = WinMD::TInclude.new(api)
